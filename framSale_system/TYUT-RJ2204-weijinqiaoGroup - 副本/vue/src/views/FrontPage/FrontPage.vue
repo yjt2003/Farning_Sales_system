01ef6c5aa9d44db9095140fe108c8f6c311df4f9
yjt2003 <3061903658@qq.com>
2025-06-22 20:49:35 +0800

    loading code

---
+++ framSale_system/TYUT-RJ2204-weijinqiaoGroup - 副本/vue/src/views/FrontPage/FrontPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="front-container" m="t-4">
+    <Carousel></Carousel>
+    <Search></Search>
+    <OurService></OurService>
+    <HotSales></HotSales>
+    <AboutUs />
+
+    <div class="bottom-menu-container" >
+      <div class="bottom-menu-box el-icon-location">
+        地址: 太原理工大学软件学院
+      </div>
+      <div class="bottom-menu-box el-icon-phone">
+        联系电话: 18735720810
+      </div>
+      <div class="bottom-menu-box el-icon-s-comment">
+        邮箱: 1149972667@qq.com
+      </div>
+
+    </div>
+
+</div>
+
+</template>
+<script>
+import OurService from "./OurService";
+import HotSales from "./HotSales";
+import Search from "./Search";
+import Carousel from "./Carousel";
+import AboutUs from "./AboutUs";
+
+export default {
+  components:{
+    Carousel,
+    OurService,
+    HotSales,
+    Search,
+    AboutUs
+  },
+
+};
+</script>
+
+<style lang="less" scoped>
+
+.bottom-menu-container{
+  background-color: #545C64;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .bottom-menu-box{
+    width: 280px;
+    color: #ffbc49;
+    font-size:20px;
+    margin: 0px 80px;
+    text-align: center;
+  }
+}
+</style>
